252fae7e51d2537bef552e8241ffd23caef0e201
In69 <90973339+In69@users.noreply.github.com>
2021-12-17 09:45:29 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+const makepizza = function (){
+console.log("deeg");
+console.log("saus");
+console.log("uien");
+console.log("vis");
+};
+
+//using a function
+makepizza();
+makepizza();
+
+
+const makesushi = function (){
+console.log("rijst");
+console.log("zalm");
+console.log("garnalen");
+};
+
+makesushi();
+makesushi();
+makesushi();
